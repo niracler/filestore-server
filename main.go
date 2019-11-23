d5f994883841f502d52f3948b2a8546a34a081f9
Niracler <1026037967@qq.com>
2019-11-23 14:55:52 +0800

    demo

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"filestore-server/handler"
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/file/", handler.UploadHandler)
+	http.HandleFunc("/file/suc/", handler.UploadSucHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Failed to start server , err: %s", err.Error())
+	}
 	fmt.Println("Hello World")
 }
