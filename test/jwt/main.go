4e89bbd6a5beb7b756768a517c334a43c8d5008b
Niracler <1026037967@qq.com>
2019-11-28 15:29:18 +0800

    通过token获取uid

--- test/jwt/main.go
+++ test/jwt/main.go
@@ -14,7 +14,7 @@ func TestCreateToken() {
 	token, _ := util.CreateToken([]byte("SecretKey"), "YDQ", 2222, "niracler")
 	fmt.Println(token)
 
-	claims, err := util.ParseToken(token, []byte("SecretKey"))
+	claims, err := util.ParseToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjgsIndlYiI6Imdyb3VwdGVuIiwiZXhwIjoxNTc0OTI5NTQzLCJpYXQiOjE1NzQ5MjU5NDN9.cXxMKTKIlAJcjx9GBrjJVA_Ie-a1zW9dEBhicmgrxS8", []byte("onlinemusic"))
 	if nil != err {
 		fmt.Println(" err :", err)
 	}
